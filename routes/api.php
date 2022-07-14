a069a6f1254fba370da8114ce70010099639940d
Danny Pacheco <dapacheco1@espe.edu.ec>
2022-07-14 11:05:11 -0500

    route update

--- routes/api.php
+++ routes/api.php
@@ -3,7 +3,14 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::apiResource('v1/rols',App\Http\Controllers\Api\V1\RolController::class)->only('show');
-Route::apiResource('v1/genders',App\Http\Controllers\Api\V1\GenderController::class)->only('show');
-Route::apiResource('v1/persons',App\Http\Controllers\Api\V1\PersonController::class)->only('show');
-Route::apiResource('v1/usuarios',App\Http\Controllers\Api\V1\UsuarioController::class)->only('show');
+Route::apiResource('v1/rols',App\Http\Controllers\Api\V1\RolController::class)
+->only(['index','show']);
+
+Route::apiResource('v1/genders',App\Http\Controllers\Api\V1\GenderController::class)
+->only(['index','show']);
+
+Route::apiResource('v1/persons',App\Http\Controllers\Api\V1\PersonController::class)
+->only(['index','show']);
+
+Route::apiResource('v1/usuarios',App\Http\Controllers\Api\V1\UsuarioController::class)
+->only(['index','show']);
